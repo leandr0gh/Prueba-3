57cc818aa92cf3a8f517ae9898a732b9dae394ae
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-06 07:54:51 -0600

    Agregar clase empresa solo falta main

---
+++ herencia/src/herenci/Empresa.java
@@ -0,0 +1,74 @@
+
+package herenci;
+import java.util.*;
+
+public class Empresa {
+    private static ArrayList<Empleadoo> empleados = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+    
+    private static Empleadoo buscar(int codigo){
+        for (Empleadoo e : empleados) {
+            if (e.getCodigo()==codigo) {
+                return e;
+            }
+        }
+        return null;
+    }
+    
+    public static void agregarEmpleado(int codigo, String nombre, String tipo){
+        Empleadoo nuevo;
+        if (buscar(codigo)!=null) {
+            System.out.println("Ya existe un empleado con ese codigo. Codigo: "+codigo);
+            return;
+        }
+        System.out.println("Ingrese su salario");
+        double salario=sc.nextDouble();
+        
+        
+        if (tipo.equalsIgnoreCase("normal")) {
+            nuevo = new EmpleadoNormal(codigo, nombre, salario);
+        } else if (tipo.equalsIgnoreCase("Temporal")){
+        nuevo = new EmpleadoTemporal(codigo, nombre, salario);
+        } else {
+            nuevo = new EmpleadoPorVenta(codigo, nombre, salario);
+        }
+        empleados.add(nuevo);
+    }
+    
+    public static double pagarEmpleado(int codigo){
+        if (buscar(codigo)==null) {
+            return 0;
+        }
+        return buscar(codigo).pago();
+    }
+    
+    public static void registrarVenta(int codigo, double monto){
+        if (buscar(codigo)!=null && buscar(codigo) instanceof EmpleadoPorVenta) {
+            ((EmpleadoPorVenta) buscar(codigo)).actualizarVentas(monto);
+        }
+    }
+    
+    public static void setFechaFin(int codigo, int anio, int mes, int dia) {
+        if (buscar(codigo) != null && buscar(codigo) instanceof EmpleadoTemporal) {
+            ((EmpleadoTemporal) buscar(codigo)).setFinContrato(anio, mes, dia);
+        }
+    }
+    
+    public static void imprimir() {
+        int normales = 0, temporales = 0, porVenta = 0;
+ 
+        for (Empleadoo e : empleados) {
+            System.out.println(e.toString());
+ 
+            if (e instanceof EmpleadoNormal) {
+                normales++;
+            } else if (e instanceof EmpleadoTemporal) {
+                temporales++;
+            } else if (e instanceof EmpleadoPorVenta) {
+                porVenta++;
+            }
+        }
+ 
+        System.out.println(normales + "-" + temporales + "-" + porVenta);
+    }
+}
